2c88645376c0f106cdbd672c2849fe90ad7930f5
Marcos Paulo dos Santos <m.santos@tuta.io>
2026-08-06 16:49:26 -0300

    Add files via upload

---
+++ target/debug/release/deps/limpador_apt-4f619cff4a943024.d
@@ -0,0 +1,5 @@
+/run/media/msantos/Projetos/limpador_apt/target/release/deps/limpador_apt-4f619cff4a943024.d: src/main.rs
+
+/run/media/msantos/Projetos/limpador_apt/target/release/deps/limpador_apt-4f619cff4a943024: src/main.rs
+
+src/main.rs:
---
+++ target/debug/release/limpador_apt.d
@@ -0,0 +1,1 @@
+/run/media/msantos/Projetos/limpador_apt/target/release/limpador_apt: /run/media/msantos/Projetos/limpador_apt/src/main.rs
