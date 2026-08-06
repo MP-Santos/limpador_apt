9dc31eb72f1f607e72bb8155d42ab4097c76bc29
Marcos Paulo dos Santos <m.santos@tuta.io>
2026-08-06 16:51:35 -0300

    Add files via upload

---
+++ target/debug/x86_64-unknown-linux-musl/release/deps/limpador_apt-72ac6b1518e0a301.d
@@ -0,0 +1,5 @@
+/run/media/msantos/Projetos/limpador_apt/target/x86_64-unknown-linux-musl/release/deps/limpador_apt-72ac6b1518e0a301.d: src/main.rs
+
+/run/media/msantos/Projetos/limpador_apt/target/x86_64-unknown-linux-musl/release/deps/limpador_apt-72ac6b1518e0a301: src/main.rs
+
+src/main.rs:
---
+++ target/debug/x86_64-unknown-linux-musl/release/limpador_apt.d
@@ -0,0 +1,1 @@
+/run/media/msantos/Projetos/limpador_apt/target/x86_64-unknown-linux-musl/release/limpador_apt: /run/media/msantos/Projetos/limpador_apt/src/main.rs
